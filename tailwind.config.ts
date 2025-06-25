a96f38a66016ce30cc7b0df22d49eb710d993312
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-25 16:13:06 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,13 +61,18 @@ export default {
 					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
 					border: 'hsl(var(--sidebar-border))',
 					ring: 'hsl(var(--sidebar-ring))'
-				}
+				},
+				success: '#13A10E',
+				accentSecondary: '#005A9E'
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			fontFamily: {
+        		sans: ["Inter", "system-ui", "sans-serif"],
+      		},
 			keyframes: {
 				'accordion-down': {
 					from: {
